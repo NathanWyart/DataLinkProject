e11852b842a747c2da4fdf9026de1b267b65b401
Pierrebvn <pierre.boivin@viacesi.fr>
2023-12-05 11:03:20 +0100

    Ajout CLStats

---
+++ CLStats.h
@@ -0,0 +1,25 @@
+#pragma once
+
+ref class CLStats {
+	
+private:
+	/*int moy_basket;
+	int turnover;
+	int under_treshold;
+	int tot_purchase;
+	System::String^ best_sell;
+	System::String^ less_sell;
+	int commercial_value; //changer le int
+	int purchase_value;
+	int variation; */
+
+public:
+	int moy_basket();
+	int turnover();
+	int under_treshold();
+	System::String^ best_sell();
+	System::String^ less_sell();
+	int commercial_value();
+	int purchse_value();
+	int variation();
+};
