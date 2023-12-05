33f677c5a6746f78429c1e3308c1e69ef6050963
Pierrebvn <pierre.boivin@viacesi.fr>
2023-12-05 10:22:20 +0100

    Ajout CLStaff.h

---
+++ CLStaff.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "CLPerson.h"
+#include "CLAddress.h"
+
+ref class CLStaff : public CLPerson {
+
+private:
+	int Id_Staff;
+	CLAddress^ staff_address;
+	System::String^ hired_date;
+	CLStaff^ superior;
+public:
+
+	//*******Setup les infos Staff**************
+	void setId_Staff(int);
+	void setstaff_address(CLAddress^ staff_address);
+	void sethire_date(System::String^ hire_date);
+	void setsuperior(int);
+
+
+	//*********Récuperer les infos Staff*************
+	int getId_Staff();
+	CLAddress^ getstaff_address(CLAddress^ staff_address);
+	System::String^ gethire_date(System::String^ hire_date);
+	System::String^ getsuperior(System::String^ superior);
+};
