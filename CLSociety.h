e50555796669d4de3225c3f4002ddcec609c80ce
Pierrebvn <pierre.boivin@viacesi.fr>
2023-12-05 10:21:25 +0100

    Ajout CLSociety.h

---
+++ CLSociety.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "CLAddress.h"
+
+ref class CLSociety {
+
+private:
+	int Id_Society;
+	System::String^ society_name;
+	CLAddress^ society_address;
+	int service_phone;
+public:
+	void setId_Society(int);
+	void setsociety_name(System::String^ society_name);
+	void setsociety_address(CLAddress^ society_address);
+	void setservice_phone(int);
+
+	int getId_Society();
+	System::String^ getsociety_name(System::String^ society_name);
+	CLAddress^ getsociety_address(CLAddress^ society_address);
+	int getservice_phone(int);
+
+};
