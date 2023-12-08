fcb36a12eb827bdef535d2f09895a67c9df5ce7f
Martin WROBLEWSKI <martin.wroblewski@viacesi.fr>
2023-12-08 09:16:09 +0100

    Correction_Staff

--- CLStaff.cpp
+++ CLStaff.cpp
@@ -15,9 +15,14 @@ void CLStaff::sethire_date(System::String^ hire_date) {
 	this->hire_date = hire_date;
 }
 
-void CLStaff::setsuperior(System::String^ superior) {
-	this->superior = superior;
-}
+void CLStaff::setfirst_name_sup(System::String^ first_name_sup) {
+	this->first_name_sup = first_name_sup;
+};
+
+void CLStaff::setlast_name_sup(System::String^ last_name_sup) {
+	this->last_name_sup = last_name_sup;
+};
+
 
 
 //**********Récupération infos staff************
@@ -33,6 +38,10 @@ System::String^ CLStaff::gethire_date(System::String^ hire_date) {
 	return this->hire_date;
 }
 
-System::String^ CLStaff::getsuperior(System::String^ superior) {
-	return this->superior;
-}
+System::String^ CLStaff::getfirst_name_sup() {
+	return first_name_sup;
+};
+
+System::String^ CLStaff::getlast_name_sup() {
+	return last_name_sup;
+};
--- CLStaff.h
+++ CLStaff.h
@@ -8,19 +8,23 @@ private:
 	int Id_Staff;
 	CLAddress^ staff_address;
 	System::String^ hire_date;
-	System::String^ superior;
+	System::String^ first_name_sup;
+	System::String^ last_name_sup;
+
 public:
 
 	//*******Setup les infos Staff**************
 	void setId_Staff(int);
 	void setstaff_address(CLAddress^ staff_address);
 	void sethire_date(System::String^ hire_date);
-	void setsuperior(System::String^ superior);
+	void setfirst_name_sup(System::String^ first_name_sup);
+	void setlast_name_sup(System::String^ last_name_sup);
 
 
 	//*********Récuperer les infos Staff*************
 	int getId_Staff();
 	CLAddress^ getstaff_address(CLAddress^ staff_address);
 	System::String^ gethire_date(System::String^ hire_date);
-	System::String^ getsuperior(System::String^ superior);
+	System::String^ getfirst_name_sup();
+	System::String^ getlast_name_sup();
 };
