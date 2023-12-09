f861bd0dd17f7609577ef2edc50b41b89cd6d026
Martin WROBLEWSKI <martin.wroblewski@viacesi.fr>
2023-12-09 17:53:21 +0100

    Correction class Address + Ajout du Form InsertStaff

--- CLAddress.h
+++ CLAddress.h
@@ -6,8 +6,10 @@ private:
 	System::String^ country;
 	System::String^ city;
 	System::String^ street;
-	System::String^ street_number;
-	System::String^ postal;
+	int street_number;
+	int postal;
+	int id_city;
+
 public:
 
 	//***setup de l'adresse***
@@ -15,16 +17,18 @@ public:
 	virtual void setcountry(System::String^ country);
 	virtual void setcity(System::String^ city);
 	virtual void setstreet(System::String^ street);
-	virtual void setstreet_number(System::String^ street_number);
-	virtual void setpostal(System::String^ postal);
+	virtual void setstreet_number(int);
+	virtual void setpostal(int);
+	virtual void setIdCity(int);
 
 	//***Récup. l'adresse***
 	virtual int getIdAddress();
 	virtual System::String^ getcountry();
 	virtual System::String^ getcity();
 	virtual System::String^ getstreet();
-	virtual System::String^ getstreet_number();
-	virtual System::String^ getpostal();
+	virtual int getstreet_number();
+	virtual int getpostal();
+	virtual int getIdCity();
 
 	//***Undéfinit***
 	//virtual static System::String^ avalue = "undefined";
