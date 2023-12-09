f86f29f889f749cfa6a2abd265834891845bc127
Pierrebvn <pierre.boivin@viacesi.fr>
2023-12-09 19:46:32 +0100

    ajout attributs et méthodes dans CLSTATS

--- CLStats.cpp
+++ CLStats.cpp
@@ -1,6 +1,38 @@
 #include "pch.h"
 #include "CLStats.h"
 
+void CLStats::settva(int tva) {
+	this->tva = tva;
+}
+
+void CLStats::setmargin(int margin) {
+	this->margin = margin;
+}
+
+void CLStats::setdiscount(int discount) {
+	this->discount = discount;
+}
+
+void CLStats::setbrand(int brand) {
+	this->brand = brand;
+}
+
+int CLStats::gettva() {
+	return this->tva;
+}
+
+int CLStats::getmargin() {
+	return this->margin;
+}
+
+int CLStats::getdiscount() {
+	return this->discount;
+}
+
+int CLStats::getbrand() {
+	return this->brand;
+}
+
 int CLStats::moy_basket() {
 	return 0;
 	;
--- CLStats.h
+++ CLStats.h
@@ -1,8 +1,23 @@
 #pragma once
 
 ref class CLStats {
+private:
+	int tva;
+	int margin;
+	int discount;
+	int brand;
 
 public:
+	void settva(int tva);
+	void setmargin(int margin);
+	void setdiscount(int discount);
+	void setbrand(int brand);
+
+	int gettva();
+	int getmargin();
+	int getdiscount();
+	int getbrand();
+
 	int moy_basket();
 	int turnover();
 	int under_treshold();
