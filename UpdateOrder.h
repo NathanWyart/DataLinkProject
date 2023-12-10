d0759afc4c51b5521427fd7e9dc34e17bdfc0f9e
Nathan WYART <nathan.wyart@viacesi.fr>
2023-12-11 00:33:13 +0100

    Ajout UpdateOrder Form

---
+++ UpdateOrder.h
@@ -0,0 +1,68 @@
+#pragma once
+
+namespace CppCLRWinFormsProject {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Description résumée de UpdateOrder
+	/// </summary>
+	public ref class UpdateOrder : public System::Windows::Forms::Form
+	{
+	public:
+		UpdateOrder(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: ajoutez ici le code du constructeur
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Nettoyage des ressources utilisées.
+		/// </summary>
+		~UpdateOrder()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Variable nécessaire au concepteur.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+		/// le contenu de cette méthode avec l'éditeur de code.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// UpdateOrder
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(282, 253);
+			this->Name = L"UpdateOrder";
+			this->Text = L"UpdateOrder";
+			this->Load += gcnew System::EventHandler(this, &UpdateOrder::UpdateOrder_Load);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void UpdateOrder_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	};
+}
